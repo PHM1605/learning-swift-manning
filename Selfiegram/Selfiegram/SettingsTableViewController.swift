bf440937c49380642e83ed85f8de2395167aedea
Pham Hoang Minh <phamhoangminh@Phams-MacBook-Pro.local>
2026-05-19 09:59:34 +0700

    updates

---
+++ Selfiegram/Selfiegram/SettingsTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsTableViewController.swift
+//  Selfiegram
+//
+//  Created by Pham Hoang Minh on 19/5/26.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
